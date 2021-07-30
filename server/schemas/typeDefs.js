61f93dcf79a5d4cbc387b30ea54b82a4100a7aaf
Emily Crook <Emily.A.Crook96@gmail.com>
2021-07-29 20:48:33 -0500

    typeDef set up for activity

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,16 @@
+const { gql } = require('apollo-server-express');
+
+// create the typeDefs
+const typeDefs = gql`
+    type Activity {
+        activityId: ID!
+        activityName: String!
+        description: String!
+        materials: [String!]
+        setUpTime: Int!
+        tearDownTime: Int!
+    }
+`;
+
+// export typeDefs
+module.exports = typeDefs;
