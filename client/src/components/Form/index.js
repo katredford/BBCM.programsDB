09c40ca5be7a91fa947765907798dc5c5c90aa47
Becca Wyland <rwyland@uwalumni.com>
2021-08-01 17:37:05 -0500

    pseudocode form element

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -3,7 +3,37 @@ import React from 'react';
 
 function ActivityForm() {
     return (
+        <section>
+            <h1>Enter Activity Info</h1>
 
+            <form id="activity-form">
+                {/* activity name input */}
+                {/* can we hide the label when it comes to form output? */}
+
+                {/* description textarea */}
+
+                {/* mess index 1-5 rating scale */}
+
+                {/* safety index 1-5 rating scale */}
+
+                {/* set up time dropdown with min ranges */}
+
+                {/* tear down time dropdown with min ranges*/}
+
+                {/* tools/materials needed checklist */}
+
+                {/* prompts maybe like add an extra field? */}
+
+                {/* learning objectives maybe like add an extra field? */}
+
+                {/* upload image - cloudinary? */}
+
+                {/* can we hide the label when it comes to form output? */}
+                {/* category/hashtag? need data type */}
+
+                
+            </form>
+        </section>
     );
 }
 
