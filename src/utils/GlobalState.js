ea79f3bd06f7481243023a191683c08e7a6c12a9
Coco Puff <fahrija_wyroski@live.com>
2021-08-02 14:19:30 -0500

    update fe

--- src/utils/GlobalState.js
+++ src/utils/GlobalState.js
@@ -1,19 +1,19 @@
-// import React, { createContext, useContext } from "react";
-// import { useActivityReducer } from "./reducers";
+import React, { createContext, useContext } from "react";
+import { useActivityReducer } from "./reducers";
 
-// const StoreContext = createContext();
-// const { Provider } = StoreContext;
+const StoreContext = createContext();
+const { Provider } = StoreContext;
 
-// const StoreProvider = ({ value = [], ...props }) => {
-//     const [state, dispatch] = useActivityReducer({
-//         activities: []
-//     });
-//     console.log('THIS IS THE STATE', state);
-//     return <Provider value={[state, dispatch]} {...props} />;
-// };
+const StoreProvider = ({ value = [], ...props }) => {
+    const [state, dispatch] = useActivityReducer({
+        activities: []
+    });
+    console.log('THIS IS THE STATE', state);
+    return <Provider value={[state, dispatch]} {...props} />;
+};
 
-// const useStoreContext = () => {
-//     return useStoreContext(StoreContext);
-// };
+const useStoreContext = () => {
+    return useStoreContext(StoreContext);
+};
 
-// export { StoreProvider, useStoreContext };
+export { StoreProvider, useStoreContext };
