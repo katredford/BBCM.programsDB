b94daf9e9b1e03907e00a08ae36b1b756e1f2efd
Coco Puff <fahrija_wyroski@live.com>
2021-08-01 20:58:50 -0500

    update home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Activity from "../components/Activity";
-import Form from "../components/Form";
+// import Activity from "../components/Activity";
+// import Form from "../components/Form";
 // import Nav from "../components/Nav";
 
 const Home = () => {
