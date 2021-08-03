f3cc4e9ab8a4afd3285c038f32e577ad06a03a0c
Becca Wyland <rwyland@uwalumni.com>
2021-08-03 09:54:49 -0500

    finish header and hero styling

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -3,7 +3,10 @@ import React from 'react';
 function Header() {
     return (
         <header id="header">
-            <h1>BBCM Programs</h1>
+            <a href="/">
+                <h1>BBCM Programs</h1>
+            </a>
+            
             <button>Add New Activity</button>
         </header>
     );
