8190c5a23002ee3b4195d4198f6b410b3f65bbcf
Becca Wyland <rwyland@uwalumni.com>
2021-08-03 19:10:47 -0500

    push

--- client/src/pages/Activity.js
+++ client/src/pages/Activity.js
@@ -6,16 +6,12 @@
 //     //   UPDATE_ACTIVITY,
 //     //   REMOVE_ACTIVITY,
 // } from "../utils/actions";
-// // import { QUERY_ACTIVITY, QUERY_ALL_ACTIVITIES } from "../utils/queries";
+// import { QUERY_ACTIVITY, QUERY_ALL_ACTIVITIES } from "../utils/queries";
 
-// function MyActivities() {
+// function myActivities() {
 //     const { id } = useParams();
 //     const [currentActivity, setCurrentActivity] = useState({});
-//     // const { loading, data } = useQuery(QUERY_ALL_ACTIVITIES);
-//     // const { activities } = state;
-
-//     return (
-//         <h1>hey</h1>
-//     );
+//     const { loading, data } = useQuery(QUERY_ALL_ACTIVITIES);
+//     const { activities } = state;
 // };
-// export default MyActivities;
+// export default myActivities;
