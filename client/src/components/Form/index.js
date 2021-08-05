f3e259d3f084670153f4b42201af96639b3d13d7
Becca Wyland <rwyland@uwalumni.com>
2021-08-04 22:58:41 -0500

    finish styling input form

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -11,94 +11,49 @@ function ActivityForm() {
         history.push("/");
     };
 
-
     return (
-        <section id="form-section">
-            <h1>Enter Activity Info</h1>
+        <section>
+            <h1 id="form-header">Enter Activity Info</h1>
 
-            <span id="form-wrapper">
-                <form id="activity-form">
+            <div id="wrapper">
+                <form className="activity-form">
                     {/* activity name input */}
-                    <span className="full-width">
-                        <label for="activity-name">Activity Name</label>
-                        <input type="text" placeholder="Activity Name" name="activity-name" id="name" class="form-input" />
-                    </span>
+                    <label for="activityName">Activity Name</label>
+                    <input type="text" placeholder="Activity Name" name="activityName" id="name" class="form-input" />
+                    
+                    {/* description textarea */}                    
+                    <label for="description">Description</label>
+                    <textarea placeholder="Description" name="description" id="description" class="form-input" rows="5"/>
+                    
+                    {/* set up time dropdown with min ranges */}                    
+                    <label for="set-up">Set Up</label>
+                    <select name="set-up" id="setup-time" class="form-input">
+                        <option value="5 minutes">5 minutes</option>
+                        <option value="10 minutes">10 minutes</option>
+                        <option value="15 minutes">15 minutes</option>
+                        <option value="20 minutes">20 minutes</option>
+                    </select>
+                        
+                    {/* tear down time dropdown with min ranges*/}                    
+                    <label for="tearDown">Tear Down</label>
+                    <select name="tearDown" className="form-input">
+                        <option value="5 minutes" className="min">5 minutes</option>
+                        <option value="10 minutes">10 minutes</option>
+                        <option value="15 minutes">15 minutes</option>
+                        <option value="20 minutes">20 minutes</option>
+                    </select>
+                    
+                    {/* tools/materials needed */}                   
+                    <label for="toolsMaterials">Tools/Materials Needed</label>
+                    <input type="input" placeholder="Tools/Materials" name="toolsMaterials" class="form-input" />
+                    
+                    {/* category/hashtag */}                    
+                    <label for="categories">Categories</label>
+                    <input type="input" placeholder="Categories" name="categories" class="form-input" />
 
-                    {/* mess index 1-5 rating scale */}
-                    <span>
-                        <label for="mess-index">Mess Index:</label>
-                        <input type="" placeholder="mess index" name="" id="" class="form-input" />
-                    </span>
-
-                    {/* set up time dropdown with min ranges */}
-                    {/* input? or, how to accept input from dropdown menu onto generated output*/}
-                    <span>
-                        <label for="set-up">Set Up</label>
-                        <select name="set-up" id="setup-time" class="form-input">
-                            <option value="5 minutes">5 minutes</option>
-                            <option value="10 minutes">10 minutes</option>
-                            <option value="15 minutes">15 minutes</option>
-                            <option value="20 minutes">20 minutes</option>
-                        </select>
-                    </span>
-
-                    {/* safety index 1-5 rating scale */}
-                    <span>
-                        <label for="">Safety Index:</label>
-                        <input type="" placeholder="safety index" name="" id="" class="form-input" />
-                    </span>
-
-                    {/* tear down time dropdown with min ranges*/}
-                    <span>
-                        <label for="tear-down">Tear Down</label>
-                        <select name="tear-down" id="tear-down" class="form-input">
-                            <option value="5 minutes">5 minutes</option>
-                            <option value="10 minutes">10 minutes</option>
-                            <option value="15 minutes">15 minutes</option>
-                            <option value="20 minutes">20 minutes</option>
-                        </select>
-                    </span>
-
-                    {/* description textarea */}
-                    <span className="full-width">
-                        <label for="description">Description:</label>
-                        <textarea placeholder="Description" name="description" id="description" class="form-input" />
-                    </span>
-
-                    {/* tools/materials needed checklist */}
-                    <span>
-                        <label for="">Tools/Materials Needed:</label>
-                        <input type="" placeholder="tools/materials" name="" id="" class="form-input" />
-                    </span>
-
-                    {/* prompts maybe like add an extra field? */}
-                    <span>
-                        <label for="">Prompts:</label>
-                        <input type="" placeholder="prompts" name="" id="" class="form-input" />
-                    </span>
-
-                    {/* learning objectives maybe like add an extra field? */}
-                    <span>
-                        <label for="">Learning Objectives:</label>
-                        <input type="" placeholder="" name="" id="" class="form-input" />
-                    </span>
-
-                    {/* upload image - cloudinary? */}
-                    <span>
-                        <label for="">Upload Image</label>
-                        <input type="" placeholder="" name="" id="" class="form-input" />
-                    </span>
-
-                    {/* can we hide the label when it comes to form output? */}
-                    {/* category/hashtag? need data type */}
-                    <span>
-                        <label for="">Categories/Hashtags</label>
-                        <input type="" placeholder="" name="" id="" class="form-input" />
-                    </span>
-
-                    <button type="submit" className="full-width" onClick={handleSubmit}>submit</button>
+                    <button type="submit" onClick={handleSubmit}>Submit!</button>
                 </form>
-            </span>
+            </div>
         </section>
     );
 }
