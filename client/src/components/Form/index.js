d4198db6c31f174d341c6877c3f21078136adb27
Becca Wyland <rwyland@uwalumni.com>
2021-08-05 01:37:31 -0500

    style login page

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -13,21 +13,21 @@ function ActivityForm() {
 
     return (
         <section>
-            <h1 id="form-header">Enter Activity Info</h1>
+            <h1 className="form-header">Enter Activity Info</h1>
 
-            <div id="wrapper">
+            <div className="activity-wrapper">
                 <form className="activity-form">
                     {/* activity name input */}
                     <label for="activityName">Activity Name</label>
-                    <input type="text" placeholder="Activity Name" name="activityName" id="name" class="form-input" />
+                    <input type="text" placeholder="Activity Name" name="activityName" id="name" className="form-input" />
                     
                     {/* description textarea */}                    
                     <label for="description">Description</label>
-                    <textarea placeholder="Description" name="description" id="description" class="form-input" rows="5"/>
+                    <textarea placeholder="Description" name="description" id="description" className="form-input" rows="5"/>
                     
                     {/* set up time dropdown with min ranges */}                    
                     <label for="set-up">Set Up</label>
-                    <select name="set-up" id="setup-time" class="form-input">
+                    <select name="set-up" id="setup-time" className="form-input">
                         <option value="5 minutes">5 minutes</option>
                         <option value="10 minutes">10 minutes</option>
                         <option value="15 minutes">15 minutes</option>
@@ -37,7 +37,7 @@ function ActivityForm() {
                     {/* tear down time dropdown with min ranges*/}                    
                     <label for="tearDown">Tear Down</label>
                     <select name="tearDown" className="form-input">
-                        <option value="5 minutes" className="min">5 minutes</option>
+                        <option value="5 minutes">5 minutes</option>
                         <option value="10 minutes">10 minutes</option>
                         <option value="15 minutes">15 minutes</option>
                         <option value="20 minutes">20 minutes</option>
@@ -45,11 +45,11 @@ function ActivityForm() {
                     
                     {/* tools/materials needed */}                   
                     <label for="toolsMaterials">Tools/Materials Needed</label>
-                    <input type="input" placeholder="Tools/Materials" name="toolsMaterials" class="form-input" />
+                    <input type="input" placeholder="Tools/Materials" name="toolsMaterials" className="form-input" />
                     
                     {/* category/hashtag */}                    
                     <label for="categories">Categories</label>
-                    <input type="input" placeholder="Categories" name="categories" class="form-input" />
+                    <input type="input" placeholder="Categories" name="categories" className="form-input" />
 
                     <button type="submit" onClick={handleSubmit}>Submit!</button>
                 </form>
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -30,40 +30,47 @@ function Login(props) {
     };
 
     return (
-        <div className="container my-1 login">
-            <h2>Login</h2>
-            <form onSubmit={handleFormSubmit}>
-                <div className="flex-row space-between my-2">
-                    <label htmlFor="email">Email address:</label>
-                    <input
-                        placeholder="youremail@test.com"
-                        name="email"
-                        type="email"
-                        id="email"
-                        onChange={handleChange}
-                    />
-                </div>
-                <div className="flex-row space-between my-2">
-                    <label htmlFor="pwd">Password:</label>
-                    <input
-                        placeholder="******"
-                        name="password"
-                        type="password"
-                        id="pwd"
-                        onChange={handleChange}
-                    />
-                </div>
-                {
-                    error ? <div>
-                        <p className="error-text" >The provided credentials are incorrect</p>
-                    </div> : null
-                }
-                <div className="flex-row flex-end">
-                    <button type="submit">
-                        Submit
-          </button>
-                </div>
-            </form>
+        <div>
+            <h2 className="login-header">Login</h2>
+
+            <div className="login-wrapper">
+                <form className="login-form" onSubmit={handleFormSubmit}>
+                        <label htmlFor="email">Email address:</label>
+                        <input
+                            placeholder="youremail@test.com"
+                            name="email"
+                            type="email"
+                            className="form-input"
+                            id="email"
+                            onChange={handleChange}
+                        />
+
+
+                   
+                        <label htmlFor="pwd">Password:</label>
+                        <input
+                            placeholder="******"
+                            name="password"
+                            type="password"
+                            className="form-input"
+                            id="pwd"
+                            onChange={handleChange}
+                        />
+                  
+
+                    {
+                        error ? <div>
+                            <p className="error-text" >The provided credentials are incorrect</p>
+                        </div> : null
+                    }
+
+                    
+                        <button type="submit">
+                            Submit!
+                        </button>
+                    
+                </form>
+            </div>
         </div>
     );
 }
